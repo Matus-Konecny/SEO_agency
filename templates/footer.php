8736d4ad67a4295fb17be0b00aadfb83488bf584
Matus-Konecny <matus.konecny@student.ukf.sk>
2025-03-30 19:59:21 +0200

    Header a footer

---
+++ templates/footer.php
@@ -0,0 +1,26 @@
+<footer>
+    <div class="container">
+      <div class="col-lg-12">
+        <p>Copyright © 2036 <a href="#">Tale SEO Agency</a>. All rights reserved. 
+        
+        <br>Design: <a href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+
+  </body>
+
+</html>
